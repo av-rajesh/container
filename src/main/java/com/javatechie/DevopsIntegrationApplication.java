7d6dd8f84a0eb18086a4bb03161e3a5200f0cef5
Venkata Rajesh Akashpu <av.rajesh@ibm.com>
2023-07-28 16:09:24 +0530

    Second Commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to javatechie again";
 	}
 
 	public static void main(String[] args) {
